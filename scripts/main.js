a4b959bc50369a7dcee5297f1118e55ef22b2a20
Ak9312 <129245931+Ak9312@users.noreply.github.com>
2024-04-07 10:32:12 +0530

    Added basic navbar and media queries

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,8 @@
 //   body.appendChild(img);
 // });
 
+window.addEventListener("load", () => {
+  const navBarHeight = document.querySelector(".NavBarContainer").offsetHeight;
+  const content = document.querySelector(".Content");
+  content.style.marginTop = navBarHeight + "px";
+});
